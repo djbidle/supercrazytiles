2485371cd3d96e978c27a99434811dfac4b02901
David Bidle <djbidle@my1026.com>
2017-02-23 20:18:53 -0500

    add tutorial button and layouts.

--- app/src/main/java/net/zacharybest/supercrazytiles/DisplayTutorialActivity.java
+++ app/src/main/java/net/zacharybest/supercrazytiles/DisplayTutorialActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 /**
- * The activity for the tutorial
+ * The activity for the tutorial_page_welcome
  * Created by Zac on 2/12/2017.
  */
 
@@ -12,6 +12,6 @@ public class DisplayTutorialActivity extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.tutorial);
+        setContentView(R.layout.tutorial_page_welcome);
     }
 }
